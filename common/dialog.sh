801dec81c8389f33ac19848c1e94598ca7827db7
bol-van <none@none.none>
2026-01-16 14:18:26 +0300

    ask_list default value fix

--- common/dialog.sh
+++ common/dialog.sh
@@ -36,7 +36,7 @@ ask_list()
 	# $3 - (optional) default value
 	local M_DEFAULT
 	eval M_DEFAULT="\$$1"
-	local M_ALL=$M_DEFAULT
+	local M_DEFAULT_VAR="$M_DEFAULT"
 	local M="" m
 
 	[ -n "$3" ] && { find_str_in_list "$M_DEFAULT" "$2" || M_DEFAULT="$3" ;}
@@ -53,5 +53,5 @@ ask_list()
 	echo selected : $M
 	eval $1="\"$M\""
 
-	[ "$M" != "$M_DEFAULT" ]
+	[ "$M" != "$M_DEFAULT_VAR" ]
 }
