aa13a1f5d27d8d9c7004a6aa8365ecbf035c537e
bol-van <none@none.none>
2026-01-16 14:57:59 +0300

    blockcheck2: oob --wf-tcp-empty=1 in windows

--- blockcheck2.d/standard/17-oob.sh
+++ blockcheck2.d/standard/17-oob.sh
@@ -5,10 +5,13 @@ pktws_oob()
 	# $1 - test function
 	# $2 - domain
 
-	local dropacks urp
+	local dropacks urp wfempty
 	for dropack in '' ':drop_ack'; do
 		for urp in b 0 2 midsld; do
-			pktws_curl_test_update "$1" "$2" --in-range=-s1 --lua-desync=oob:urp=$urp$dropack
+			# drop_ack cannot drop anything if it does not come. windows default --wf-tcp-empty=0 stop empty ACKs
+			wfempty=
+			[ -n "$dropack" -a "$UNAME" = CYGWIN ] && wfempty="--wf-tcp-empty=1"
+			pktws_curl_test_update "$1" "$2" $wfempty --in-range=-s1 --lua-desync=oob:urp=$urp$dropack
 		done
 	done
 }
